f96dde751d80d3d8dd9959f89b3762e5bba688eb
apurvaKhade25 <apurvakhade259@gmail.com>
2026-05-18 17:36:56 +0530

    CompanyController edited comment out

--- src/main/java/com/jobboard/job_board/company/CompanyController.java
+++ src/main/java/com/jobboard/job_board/company/CompanyController.java
@@ -16,22 +16,26 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
+    //create company
     @PostMapping()
     public ResponseEntity <CompanyResponse> createCompany(@Valid @RequestBody CompanyRequest companyRequest){
         return ResponseEntity.status(201).body(companyService.createCompany(companyRequest));
 
     }
 
+    //get by company
     @GetMapping("/{id}")
     public ResponseEntity <CompanyResponse> getHistory(@PathVariable Long id){
         return ResponseEntity.status(201).body(companyService.getHistory(id));
     }
 
+    //get all companies
     @GetMapping()
     public ResponseEntity <List<CompanyResponse>> getEverything(){
         return ResponseEntity.ok((companyService.getEverything()));
     }
 
+    //delete by id
     @DeleteMapping("/delete/{id}")
     public ResponseEntity <String> delete(@PathVariable Long id){
         companyService.deleteCompany(id);
