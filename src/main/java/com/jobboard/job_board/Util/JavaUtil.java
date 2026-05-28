664505fdc9fc24aeb61031a85c32a02e69efe023
apurvaKhade25 <apurvakhade259@gmail.com>
2026-05-29 00:38:02 +0530

    JavaUtil added

---
+++ src/main/java/com/jobboard/job_board/Util/JavaUtil.java
@@ -0,0 +1,24 @@
+package com.jobboard.job_board.Util;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+@Component
+public class JavaUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    // convert secret string to Key object
+    private Key getSigningKey(){
+        byte[] keyBytes=secret.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
