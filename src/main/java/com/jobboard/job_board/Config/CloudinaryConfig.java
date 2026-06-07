7e61191f12c9522393147b47c6334406f3a536cf
apurvaKhade25 <apurvakhade259@gmail.com>
2026-06-08 00:47:36 +0530

    Added: CloudinaryConfig

--- src/main/java/com/jobboard/job_board/Config/CloudinaryConfig.java
+++ src/main/java/com/jobboard/job_board/Config/CloudinaryConfig.java
@@ -1,4 +1,32 @@
 package com.jobboard.job_board.Config;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud-name}")
+    public String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    public String apiKey;
+
+    @Value(("${cloudinary.api-secret}"))
+    public String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(Map.of(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
+                "secure", true
+        ));
+    }
+
 }
